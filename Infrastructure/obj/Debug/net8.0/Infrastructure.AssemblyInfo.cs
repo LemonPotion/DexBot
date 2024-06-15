d086f1c48453dd8ca676b12bff1c01cd354576f4
LemonPotion <140953841+LemonPotion@users.noreply.github.com>
2024-06-16 01:51:56 +0300

    OpenAISettings.cs added; TelegramSettings.cs changed; appsettings.json changed;

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a57a262fc5d98085be085d82f5fe8a63d5dc7f60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecb6a609ff8ce3f9632208a1faf91350de0f2842")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
