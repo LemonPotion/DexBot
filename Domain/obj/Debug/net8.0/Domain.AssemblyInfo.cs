e91c2de6adf0416cdb20dc4de894e169b765c245
LemonPotion <140953841+LemonPotion@users.noreply.github.com>
2024-06-15 18:15:52 +0300

    Quartz added; PersonFindBirthdayJob.cs implemented; CronExpressionSettings.cs added; TelegramSettings.cs added; Quartz config added; Validation Fixed;

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17d5fa835ae5102394c5040e9e1c49596e7a2623")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fedb93e2a37aa212f5aa397595f969d8b1638a84")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
