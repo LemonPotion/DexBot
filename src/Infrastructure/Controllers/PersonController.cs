0b49a4f65c110435ab2497f22a794e70a7f3ff32
LemonPotion <140953841+LemonPotion@users.noreply.github.com>
2024-06-17 19:03:56 +0300

    Logger added; PersonController.cs updated;

--- src/Infrastructure/Controllers/PersonController.cs
+++ src/Infrastructure/Controllers/PersonController.cs
@@ -1,5 +1,6 @@
 using Application.Dto_s.Person.Requests;
 using Application.Services;
+using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Infrastructure.Controllers;
@@ -10,6 +11,13 @@ namespace Infrastructure.Controllers;
 [Route("/api/[controller]")]
 public class PersonController : ControllerBase
 {
+    private readonly ILogger<PersonController> _logger;
+
+    public PersonController(ILogger<PersonController> logger)
+    {
+        _logger = logger;
+    }
+
     /// <summary>
     /// Create request
     /// </summary>
@@ -17,12 +25,27 @@ public class PersonController : ControllerBase
     /// <param name="service"></param>
     /// <param name="cancellationToken"></param>
     /// <returns>IAcationResult</returns>
-    [HttpPost("create")]
+    [HttpPost]
     public async Task<IActionResult> CreateAsync([FromBody] CreatePersonRequest request,[FromServices] PersonService service, CancellationToken cancellationToken)
     {
         
-        return Ok(await service.CreateAsync(request,cancellationToken));
-        
+        _logger.LogInformation("Received create person request");
+        try
+        {
+            var result = await service.CreateAsync(request, cancellationToken);
+            _logger.LogInformation($"Successfully created person with id {result.Id}");
+            return Ok(result);
+        }
+        catch (ValidationException e)
+        {
+            _logger.LogWarning("Validation error occurred while creating a person");
+            return BadRequest(e.Message);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error occurred while creating a person");
+            return StatusCode(500, "Internal server error");
+        }
     }
     /// <summary>
     /// Get by id request
@@ -31,10 +54,25 @@ public class PersonController : ControllerBase
     /// <param name="service"></param>
     /// <param name="cancellationToken"></param>
     /// <returns>IactionResult</returns>
-    [HttpGet("getById")]
+    [HttpGet("{id:guid}")]
     public async Task<IActionResult> GetByIdAsync(Guid id, [FromServices] PersonService service, CancellationToken cancellationToken)
     {
-        return Ok(await service.GetByIdAsync(id, cancellationToken));
+        _logger.LogInformation($"Received request to get person by id {id}");
+        try
+        {
+            var result = await service.GetByIdAsync(id, cancellationToken);
+            if (result is null)
+            {
+                return NotFound();
+            }
+            _logger.LogInformation($"Successfully retrieved person with id {id}");
+            return Ok(result);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error occurred while getting person by id {Id}", id);
+            return StatusCode(500, "Internal server error");
+        }
     }
     /// <summary>
     /// Update request
@@ -43,10 +81,26 @@ public class PersonController : ControllerBase
     /// <param name="service"></param>
     /// <param name="cancellationToken"></param>
     /// <returns>IActionResult</returns>
-    [HttpPut("update")]
+    [HttpPut]
     public async Task<IActionResult> UpdateAsync([FromBody] UpdatePersonRequest request, [FromServices] PersonService service, CancellationToken cancellationToken)
     {
-        return Ok(await service.UpdateAsync(request,cancellationToken));
+        _logger.LogInformation($"Received request to update a person with id {request.Id}");
+        try
+        {
+            var result = await service.UpdateAsync(request,cancellationToken);
+            _logger.LogInformation($"Successfully updated person with id {result.Id}");
+            return Ok(result);
+        }
+        catch (ValidationException e)
+        {
+            _logger.LogWarning($"Validation error occurred while updating a person");
+            return BadRequest(e.Message);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, $"Error occurred while updating person with id {request.Id}");
+            return StatusCode(500, "Internal server error");
+        }
     }
     /// <summary>
     /// Delete request
@@ -55,10 +109,29 @@ public class PersonController : ControllerBase
     /// <param name="service"></param>
     /// <param name="cancellationToken"></param>
     /// <returns></returns>
-    [HttpDelete("delete")]
+    [HttpDelete]
     public async Task<IActionResult> DeleteAsync([FromBody] DeletePersonRequest request, [FromServices] PersonService service, CancellationToken cancellationToken)
     {
-        return Ok(await service.DeleteByIdAsync(request,cancellationToken));
+        _logger.LogInformation($"Received request to delete person with id {request.Id}");
+        try
+        {
+            var result = await service.DeleteByIdAsync(request, cancellationToken);
+            if (result)
+            {
+                _logger.LogInformation($"Successfully deleted person with id {request.Id}");
+                return Ok(result);
+            }
+            else
+            {
+                _logger.LogInformation($"Unsuccessfully deleted person with id {request.Id}");
+                return BadRequest();
+            }
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, $"Error occurred while deleting person with id {request.Id}");
+            return StatusCode(500, "Internal server error");
+        }
     }
     /// <summary>
     /// Get all request
@@ -66,9 +139,21 @@ public class PersonController : ControllerBase
     /// <param name="service"></param>
     /// <param name="cancellationToken"></param>
     /// <returns></returns>
-    [HttpGet("get")]
+    [HttpGet("getAll")]
     public async Task<IActionResult> GetAsync([FromServices] PersonService service, CancellationToken cancellationToken)
     {
-        return Ok(await service.GetAll(cancellationToken));
+        _logger.LogInformation("Received request to get all persons");
+        
+        try
+        {
+            var result = await service.GetAll(cancellationToken);
+            _logger.LogInformation("Successfully retrieved all persons");
+            return Ok(result);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error occurred while getting all persons");
+            return StatusCode(500, "Internal server error");
+        }
     }
 }
